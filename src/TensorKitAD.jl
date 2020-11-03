43f5cfacc733428ff07cbe85a3ced25fe02aeba9
maarten <maarten.vandamme@ugent.be>
2020-11-03 15:58:21 +0100

    added krylovkit + non working inf mps

--- src/TensorKitAD.jl
+++ src/TensorKitAD.jl
@@ -1,7 +1,8 @@
 module TensorKitAD
-    using TensorKit, TensorOperations, ChainRulesCore, TupleTools
+    using TensorKit, TensorOperations, ChainRulesCore, TupleTools, KrylovKit
 
     include("linalg.jl")
     include("tensoroperations.jl")
+    include("krylovkit.jl")
 
 end # module

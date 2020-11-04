3059c5edf0b13410975387a2301998bed596de2c
maarten <maarten.vandamme@ugent.be>
2020-11-04 17:34:14 +0100

    not working tho

--- examples/cmps.jl
+++ examples/cmps.jl
@@ -1,17 +1,14 @@
-#to run this file you'll need OMEinsum's master
 using Zygote #the autodiff engine
-
 using TensorKit, TensorKitAD
-using OMEinsum # also exports an @tensor macro, but autodiffeable
-using KrylovKit
-using LinearAlgebra
+using KrylovKit, LinearAlgebra
 
 
 D = 4
 K0 = Matrix{Float64}(I, D, D);
 R0 = Matrix{Float64}(I, D, D);
 
-function cMPSEnergy(k::Float64, m::Float64, g::Float64, K::Array{}, R::Array{}, D::Int)
+function cMPSEnergy(tup)
+    (k,m,g,K,R,D) = tup
     Q = 1im*K - 1/2*(R'*R);
     U = Matrix{Float64}(I, D, D);
     Tr0 = Matrix{Float64}(I, D, D);
@@ -19,6 +16,7 @@ function cMPSEnergy(k::Float64, m::Float64, g::Float64, K::Array{}, R::Array{}, 
     lamda,r,info_eig = eigsolve(x -> Q*x + x*Q' + R*x*R',Tr0,1,:LR,Arnoldi());
     r = r[1]/tr(r[1]) ;# Cancels complex phase of r and ensures the normalization condition tr(l*r) = 1, where l is the identity matrix
     r = (r + r')/2 ;# Ensure r is exactly Hermitian, which is necesary for eig to produce a unitary matrix Vr
+    Dr,Vr = eigen(r)
     order = sortperm(real(Dr),rev=true);
     Dr = Dr[order];
     Vr = Vr[:,order];
@@ -33,10 +31,10 @@ function cMPSEnergy(k::Float64, m::Float64, g::Float64, K::Array{}, R::Array{}, 
     # Compute initial energy
     ekin = k*real(tr(QR*r*R'));
     epot = m*real(tr(R*r*R'));
-    eint = g*real(tr(R²*r*R²))
+    eint = g*real(tr(R²*r*R²'))
     e = epot + ekin + eint;
     return e
 end
 
-@show cMPSEnergy(1.,1.,10.,K0,R0,D)
-@show cMPSEnergy'(1.,1.,10.,K0,R0,D)
+@show cMPSEnergy((1.,1.,10.,K0,R0,D))
+@show cMPSEnergy'((1.,1.,10.,K0,R0,D))

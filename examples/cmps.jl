ac99c6a7d338a5298ef3304a4f96952505835a5a
BenoitTuybens <benoit.tuybens@ugent.be>
2020-11-04 17:11:54 +0100

    cMPS example correct

--- examples/cmps.jl
+++ examples/cmps.jl
@@ -11,11 +11,24 @@ D = 4
 K0 = Matrix{Float64}(I, D, D);
 R0 = Matrix{Float64}(I, D, D);
 
-function cMPSEnergy(k::Float64, μ::Float64, g::Float64, K::Array{}, R::Array{}, D::Int)
-    Q = 1im*K - 1/2*(R1'*R1) - 1/2*(R2'*R2);
+function cMPSEnergy(k::Float64, m::Float64, g::Float64, K::Array{}, R::Array{}, D::Int)
+    Q = 1im*K - 1/2*(R'*R);
     U = Matrix{Float64}(I, D, D);
+    Tr0 = Matrix{Float64}(I, D, D);
 
-    lamda,r,info_eig = eigsolve(x -> Q*x + x*Q' + Re*x*Re' + Rp*x*Rp',Tr0,1,:LR,Arnoldi());
+    lamda,r,info_eig = eigsolve(x -> Q*x + x*Q' + R*x*R',Tr0,1,:LR,Arnoldi());
+    r = r[1]/tr(r[1]) ;# Cancels complex phase of r and ensures the normalization condition tr(l*r) = 1, where l is the identity matrix
+    r = (r + r')/2 ;# Ensure r is exactly Hermitian, which is necesary for eig to produce a unitary matrix Vr
+    order = sortperm(real(Dr),rev=true);
+    Dr = Dr[order];
+    Vr = Vr[:,order];
+    r = diagm(0 => Dr);
+    K = Vr'*K*Vr;
+    R = Vr'*R*Vr;
+
+
+    Q = 1im*K - 1/2*(R'*R);
+    QR = Q*R - R*Q
     R² = R*R;
     # Compute initial energy
     ekin = k*real(tr(QR*r*R'));
@@ -25,4 +38,5 @@ function cMPSEnergy(k::Float64, μ::Float64, g::Float64, K::Array{}, R::Array{},
     return e
 end
 
-@show cMPSEnergy'(1,1,10,K0,R0,D)
+@show cMPSEnergy(1.,1.,10.,K0,R0,D)
+@show cMPSEnergy'(1.,1.,10.,K0,R0,D)

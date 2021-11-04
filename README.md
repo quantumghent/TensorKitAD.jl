50bfd5821bb3cfb68f73b6b3072639b7f2e04777
maarten <maarten.vandamme@ugent.be>
2021-11-04 15:27:28 +0100

    d

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # TensorKitAD.jl
 
+Examples on how to derive backward rules : https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf
+
+How to define it in julia : https://github.com/JuliaDiff/ChainRules.jl
+
+The autodiff engine I'm testing with : https://github.com/FluxML/Zygote.jl
+
 ## TensorOperations.jl
 
 TensorOperations sometimes modifies memory in-place, which is forbidden for all autodiff engines. The way around this is to re-define the 'basic operations' to no longer work in-place. Things like
@@ -23,4 +29,4 @@ some basic operations are supported
 
 ## KrylovKit.jl
 
-krylovkit is tricky, because its methods often takes a function handle. In principle you can define the pullback wrt to the function handle, but the AD engine has no way to work with inproducts of generic functions.
+krylovkit is tricky, because its methods often takes a function handle. In principle you can define the pullback wrt to the function handle, but the AD engine has no way to work with inproducts of generic functions. Maybe there is a smart way around this - the easy way is to define LinearMap objects, which in turn can be made deriveable.
--- examples/finitemps.jl
+++ examples/finitemps.jl
@@ -1,39 +1,39 @@
 using Zygote,TensorKit,TensorKitAD,MPSKit
 
-let
-    #create some random mps
-    bonddims = [1,2,4,8,10,10,8,4,2,1]
-    tensors = [TensorMap(rand,ComplexF64,ℂ^bonddims[i]*ℂ^2,ℂ^bonddims[i+1]) for i in 1:length(bonddims)-1];
+#create some random mps
+bonddims = [1,2,4,8,10,10,8,4,2,1];
+tensors = [TensorMap(rand,ComplexF64,ℂ^bonddims[i]*ℂ^2,ℂ^bonddims[i+1]) for i in 1:length(bonddims)-1];
 
-    #the hamiltonian
-    sz = TensorMap(ComplexF64[1 0;0 -1],ℂ^2,ℂ^2);
-    sx = TensorMap(ComplexF64[0 1;1 0],ℂ^2,ℂ^2);
-    @tensor ham[-1 -2;-3 -4]:=sz[-1 -2]*sz[-3 -4]+0.5*sx[-1 -2]*one(sx)[-3 -4]+0.5*one(sx)[-1 -2]*sx[-3 -4];
+#the hamiltonian
+sz = TensorMap(ComplexF64[1 0;0 -1],ℂ^2,ℂ^2);
+sx = TensorMap(ComplexF64[0 1;1 0],ℂ^2,ℂ^2);
+@tensor ham[-1 -2;-3 -4]:=sz[-1 -2]*sz[-3 -4]+0.5*sx[-1 -2]*one(sx)[-3 -4]+0.5*one(sx)[-1 -2]*sx[-3 -4];
 
-    function calculate_energy(state)
+function calculate_energy(state)
 
-        l = isomorphism(Matrix{ComplexF64},space(state[1],1),space(state[1],1))
-        r = isomorphism(Matrix{ComplexF64},space(state[end],3),space(state[end],3))
+    l = isomorphism(Matrix{ComplexF64},space(state[1],1),space(state[1],1))
+    r = isomorphism(Matrix{ComplexF64},space(state[end],3),space(state[end],3))
 
-        @tensor lh[-1;-2] := l[1,2]*state[1][2,3,-2]*conj(state[1][1,3,-1])
-        lh*=0;
+    @tensor lh[-1;-2] := l[1,2]*state[1][2,3,-2]*conj(state[1][1,3,-1])
+    lh*=0;
 
-        for i in 2:length(state)
-            @tensor lh[-1;-2] := lh[1,2]*state[i][2,3,-2]*conj(state[i][1,3,-1])
-            @tensor lh[-1;-2]+= l[1,2]*state[i-1][2,3,4]*state[i][4,5,-2]*ham[6,3,7,5]*conj(state[i-1][1,6,8])*conj(state[i][8,7,-1])
-            @tensor l[-1;-2] := l[1;2]*state[i-1][2,3,-2]*conj(state[i-1][1,3,-1])
-        end
-
-        en = @tensor lh[1,2]*r[1,2]
-        norm = @tensor l[1,2]*state[end][2,3,4]*conj(state[end][1,3,4])
-
-        real(en/norm)
+    for i in 2:length(state)
+        @tensor lh[-1;-2] := lh[1,2]*state[i][2,3,-2]*conj(state[i][1,3,-1])
+        @tensor lh[-1;-2]+= l[1,2]*state[i-1][2,3,4]*state[i][4,5,-2]*ham[6,3,7,5]*conj(state[i-1][1,6,8])*conj(state[i][8,7,-1])
+        @tensor l[-1;-2] := l[1;2]*state[i-1][2,3,-2]*conj(state[i-1][1,3,-1])
     end
 
-    for step in 1:1000
-        tensors = tensors - 0.01*calculate_energy'(tensors)
-        normalize!.(tensors)
-        @show calculate_energy(tensors)
-    end
+    en = @tensor lh[1,2]*r[1,2]
+    norm = @tensor l[1,2]*state[end][2,3,4]*conj(state[end][1,3,4])
 
+    real(en/norm)
+end
+
+
+calculate_energy'(tensors)
+
+for step in 1:1000
+    tensors = tensors - 0.01*calculate_energy'(tensors)
+    normalize!.(tensors)
+    @show calculate_energy(tensors)
 end
